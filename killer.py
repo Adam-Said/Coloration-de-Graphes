c97b93f62532bd22b2702bd0592f5bb15e296251
Adam S <adam.said@etu.umontpellier.fr>
2022-11-13 16:21:40 +0100

    [AS] Correction bug affichage, baisse sleep, ajout process killer

---
+++ killer.py
@@ -0,0 +1,21 @@
+import os
+import time
+from subprocess import Popen, PIPE
+
+print("Destruction des processus")
+
+res = Popen(["ps", "aux"], stdout=PIPE)
+res = ''.join(map(chr, (res.communicate())[0]))
+
+
+for r in res.split('\n'):
+    if (("bin/client" in r) or ("bin/serverMultiplex" in r)):
+        num = r.split(' ')
+        for n in num:
+            if len(n) > 0 and n[0] in '0123456789':
+                os.system("kill " + n)
+                break
+        if "client" in r:
+            print("Killed client.")
+        else:
+            print("Killed server.")
--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 import os
 import time
+from subprocess import Popen, PIPE
 
 print("Compilation...")
 os.system("mkdir obj")
@@ -22,13 +23,33 @@ for x in f:
 print("Besoin de",nbNode , "clients")
 
 file = '"' + fileName + '"'
-print("Démarrage du serveur sur " + str(port))
+print("[Runner] Démarrage du serveur sur " + str(port))
 call = "./bin/serverMultiplex " + str(port) + " " + file +" &"
 os.system(call)
 time.sleep(1)
 for x in range(0,nbNode) :
   time.sleep(0.2)
   tmpPort = port+x+1
-  print("Démarrage du client" , x , "sur" , str(tmpPort) ,"au serveur", str(port))
+  print("[Runner] Démarrage du client" , x , "sur" , str(tmpPort) ,"au serveur", str(port))
   call = "./bin/client 127.0.0.1 " + str(port) + " " + str(tmpPort) +" &"
   os.system(call)
+  
+
+time.sleep(20)
+print("[Runner] Fin du programme\n[Runner] Destruction des processus")
+
+res = Popen(["ps", "aux"], stdout=PIPE)
+res = ''.join(map(chr, (res.communicate())[0]))
+
+
+for r in res.split('\n'):
+    if (("bin/client" in r) or ("bin/serverMultiplex" in r)):
+        num = r.split(' ')
+        for n in num:
+            if len(n) > 0 and n[0] in '0123456789':
+                os.system("kill " + n)
+                break
+        if "client" in r:
+            print("[Runner] Killed client.")
+        else:
+            print("[Runner] Killed server.")
